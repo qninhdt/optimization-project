82f7f6b24db589173f87943ac0f135ad6b0ccac4
qninhdt <qndt123@gmail.com>
2024-04-24 00:34:24 +0700

    cc

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from solver import GradientDescentSolver
 from dataset import load_dataset
+import json
+import os
 
 # Load dataset
-# dataset = load_dataset("uf20-91")
-dataset = load_dataset("uf75-325")
+DATASETS = [
+    "uf20-91",
+    "uf50-218",
+    "uf75-325",
+]
 
-# Initialize solver
-solver = GradientDescentSolver()
+MAX_SAMPLES = 100
 
-dataset["samples"] = dataset["samples"][:100]
+# create folder reports if it does not exist
+os.makedirs("reports", exist_ok=True)
 
-# Solve dataset
-solutions = solver.solve_dataset(dataset)
+
+def run_experiment(solver, dataset_name: str):
+    print(f"> Running experiment with {type(solver).__name__} on {dataset_name}")
+    os.makedirs(f"reports/{type(solver).__name__}", exist_ok=True)
+
+    dataset = load_dataset(dataset_name)
+
+    n_samples = min(MAX_SAMPLES, len(dataset["samples"]))
+    dataset["samples"] = dataset["samples"][:n_samples]
+    dataset["n_samples"] = n_samples
+
+    report = solver.solve_dataset(dataset)
+
+    with open(f"reports/{type(solver).__name__}/{dataset_name}.json", "w") as f:
+        json.dump(report, f, indent=4)
+
+
+for dataset_name in DATASETS:
+    solver = GradientDescentSolver()
+    run_experiment(solver, dataset_name)
